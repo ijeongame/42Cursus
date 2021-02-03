a031bae369dd40ef9606a0e6860e555d98680b17
kwon <gurqo1223@gmail.com>
2021-02-03 16:21:22 +0900

    parsing_format

---
+++ ft_printf/ft_parse_format.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_format.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/28 20:53:30 by hkwon             #+#    #+#             */
+/*   Updated: 2021/02/03 16:20:27 by hkwon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		ft_parse_flag(va_list ap, t_format *op)
+{
+	if (**op->str == '-')
+		op->left = 1;
+	if (**op->str == '0')
+		op->zero = 1;
+	if (**op->str == '#')
+		op->base = 1;
+	if (**op->str == ' ')
+		op->space = 1;
+	if (**op->str == '+')
+		op->sign = 1;
+	++(*op->str);
+	return (ft_parse_format(ap, op));
+}
+
+int		ft_parse_width(va_list ap, t_format *op)
+{
+	int		s_width;
+
+	s_width = 0;
+	if (**op->str == '*')
+	{
+		if ((s_width = va_arg(ap, int)) < 0)
+			s_width = -s_width;
+		++(*op->str);
+	}
+	else
+	{
+		while (ft_isdigit(**op->str))
+		{
+			s_width *= 10;
+			s_width += **op->str - '0';
+			++(*op->str);
+		}
+	}
+	op->width = s_width;
+	return (ft_parse_format(ap, op));
+}
+
+int		ft_parse_prec(va_list ap, t_format *op)
+{
+	int		s_prec;
+
+	s_prec = 0;
+	++(*op->str);
+	if (**op->str == '*')
+	{
+		if ((op->prec = va_arg(ap, int)) < 0)
+			++(*op->str);
+	}
+	else
+	{
+		while (ft_isdigit(**op->str))
+		{
+			s_prec *= 10;
+			s_prec += **op->str - '0';
+			++(*op->str);
+		}
+		op->prec = s_prec;
+	}
+	return (ft_parse_format(ap, op));
+}
+
+int		ft_parse_type(va_list ap, t_format *op)
+{
+	if (**op->str == 'c')
+		ft_print_char(ap, op);
+	if (**op->str == 's')
+		ft_print_str(ap, op);
+	if (**op->str == 'p')
+		ft_print_ptr(ap, op);
+	if (**op->str == 'd' || **op->str == 'i')
+		ft_print_int(ap, op);
+	if (**op->str == 'u')
+		ft_print_un_int(ap, op);
+	if (**op->str == '%')
+		ft_print_per(ap, op);
+	return (0);
+}
+
+int			ft_parse_format(va_list ap, t_format *op)
+{
+	if (ft_strchr("-0# +", **op->str))
+		ft_parse_flag(ap, op);
+	if (ft_strchr("123456789*", **op->str))
+		ft_parse_width(ap, op);
+	if (ft_strchr(".", **op->str))
+		ft_parse_prec(ap, op);
+	if (ft_strcht("lh", **op->str))
+		ft_parse_extend(ap, op);
+	if (ft_strchr("cspdiuxX%", **op->str))
+		ft_parse_type(ap, op);
+	return (0);
+}
