6ec43a97b22a8b1b4989b00022fec19e552bc3e7
kwon <gurqo1223@gmail.com>
2021-02-03 16:16:12 +0900

    add_parsing_extend_type

--- ft_printf/ft_parse_start.c
+++ ft_printf/ft_parse_start.c
@@ -6,11 +6,11 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 18:03:30 by hkwon             #+#    #+#             */
-/*   Updated: 2021/01/31 15:58:58 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/02/03 15:56:00 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
 t_format	*ft_set_list(const char **format, int cnt)
 {
@@ -26,23 +26,24 @@ t_format	*ft_set_list(const char **format, int cnt)
 	op->width = 0;
 	op->prec = 0;
 	op->type = 0;
-	op->len = cnt;
+	op->len_h = 0;
+	op->len_l = 0;
+	op->cnt = cnt;
 	op->str = format;
 	return (op);
 }
 
 int			ft_parse_format(va_list ap, t_format *op)
 {
-	char **format;
-
-	format = op->str;
-	if (ft_strchr("-0# +", **format))
+	if (ft_strchr("-0# +", **op->str))
 		ft_parse_flag(ap, op);
-	if (ft_strchr("123456789*", **format))
+	if (ft_strchr("123456789*", **op->str))
 		ft_parse_width(ap, op);
-	if (ft_strchr(".", **format))
-	 	ft_parse_prec(ap, op);
-	if (ft_strchr("cspdiuxX%", **format))
+	if (ft_strchr(".", **op->str))
+		ft_parse_prec(ap, op);
+	if (ft_strcht("lh", **op->str))
+		ft_parse_extend(ap, op);
+	if (ft_strchr("cspdiuxX%", **op->str))
 		ft_parse_type(ap, op);
 	return (0);
 }
