34e97ab9d9d2554a2f630fa9df52c8dc5cbc8a07
kwon <gurqo1223@gmail.com>
2021-02-03 22:44:47 +0900

    print_int_type_1

--- ft_printf/ft_print_int.c
+++ ft_printf/ft_print_int.c
@@ -6,22 +6,21 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 16:05:18 by hkwon             #+#    #+#             */
-/*   Updated: 2021/02/03 17:03:44 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/02/03 22:41:11 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int			ft_print_int(va_list ap, t_format *op)
+int		ft_print_int(va_list ap, t_format *op)
 {
 	long long int	num;
-	char			*value;
+	char			*n_str;
 	int				cnt;
 	int				len;
 
-	num = ft_extend_di(ap, op);
-	value = ft_lltoa(num);
-	len = ft_strlen(value);
-	cnt = ;
+	num = ft_extend_di(num, op);
+	n_str = ft_lltoa(num);
+
 	return (cnt);
 }
