d6580ac4a4dcb733d64b4ebcf38114eee8a87b76
kwon <gurqo1223@gmail.com>
2021-02-03 16:20:51 +0900

    parsing_start

--- ft_printf/ft_parse_start.c
+++ ft_printf/ft_parse_start.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 18:03:30 by hkwon             #+#    #+#             */
-/*   Updated: 2021/02/03 15:56:00 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/02/03 16:20:29 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,6 @@ t_format	*ft_set_list(const char **format, int cnt)
 	op->cnt = cnt;
 	op->str = format;
 	return (op);
-}
-
-int			ft_parse_format(va_list ap, t_format *op)
-{
-	if (ft_strchr("-0# +", **op->str))
-		ft_parse_flag(ap, op);
-	if (ft_strchr("123456789*", **op->str))
-		ft_parse_width(ap, op);
-	if (ft_strchr(".", **op->str))
-		ft_parse_prec(ap, op);
-	if (ft_strcht("lh", **op->str))
-		ft_parse_extend(ap, op);
-	if (ft_strchr("cspdiuxX%", **op->str))
-		ft_parse_type(ap, op);
-	return (0);
 }
 
 int			ft_parse_start(va_list ap, const char **format, int cnt)
