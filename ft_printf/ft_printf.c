4fe5ec0eedeae5e311fdb2665a4db99944508852
kwon <gurqo1223@gmail.com>
2021-02-22 01:22:14 +0900

    ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 15:37:22 by hkwon             #+#    #+#             */
-/*   Updated: 2021/02/21 21:01:30 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/02/22 01:21:50 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,18 @@ static int		ft_parse_type(char format, va_list ap, t_format *op)
 	return (0);
 }
 
-static void		ft_parse_format(char format, va_list ap, t_format *op)
+static void		ft_parse_width_prec(char format, va_list ap, t_format *op)
 {
-	//flag
-	if (format == '0' && op->zero == 0 && op->point == 0 && op->width == 0)
-		op->zero = 1;
-	else if (format == '-')
-		op->left = 1;
-	//wdith & precision
-	else if (format == '.')
+	if (ft_isdigit(format))
 	{
-		op->point = 1;
-		op->prec = 0;
+		if (op->prec == -1)
+			op->width = (op->width * 10) + format - '0';
+		else
+			op->prec = (op->prec * 10) + format - '0';
 	}
 	else if (format == '*')
 	{
-		if (op->point == 0)
+		if (op->prec == -1)
 		{
 			if ((op->width = va_arg(ap, int)) < 0)
 			{
@@ -55,49 +51,59 @@ static void		ft_parse_format(char format, va_list ap, t_format *op)
 			}
 		}
 		else
-			op->prec = va_arg(ap, int);
-	}
-	else if (ft_isdigit(format))
-	{
-		if (op->point == 0)
-			op->width = (op->width * 10) + format - '0';
-		else
-			op->prec = (op->prec * 10) + format - '0';
+		{
+			if ((op->prec = va_arg(ap, int)) < 0)
+				op->prec = -1;
+		}
 	}
 }
 
-static int		start_printf(const char *format, va_list ap, t_format *op)
+static void		ft_parse_format(char format, va_list ap, t_format *op)
 {
-	int s_cnt;
+	if (format == '0' && op->prec == -1 && op->width == 0)
+		op->zero = 1;
+	else if (format == '-')
+		op->left = 1;
+	else if (format == '.')
+		op->prec = 0;
+	else if (ft_isdigit(format) || format == '*')
+		ft_parse_width_prec(format, ap, op);
+}
+
+static int		start_printf(const char *format, va_list ap)
+{
+	int			s_cnt;
+	t_format	*op;
 
 	s_cnt = 0;
+	if (!(op = malloc(sizeof(t_format))))
+		return (-1);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			++format;
-			while (*format && ft_strchr("cspdiuxX%", *format) == 0)
+			ft_init_list(op);
+			while (*format && !ft_strchr("cspdiuxX%", *format))
 				ft_parse_format(*(format++), ap, op);
-			if (*format && ft_strchr("cspdiuxX%", *format) != 0)
-				s_cnt += ft_parse_type(*(format++), ap, op);
+			if (op->left == 1 || op->prec > -1)
+				op->zero = 0;
+			s_cnt += ft_parse_type(*(format++), ap, op);
 		}
 		else
 			s_cnt += ft_putchar(*(format++));
 	}
+	free(op);
 	return (s_cnt);
 }
 
 int		ft_printf(const char *format, ...)
 {
 	va_list		ap;
-	t_format	*op;
 	int			cnt;
 
 	va_start(ap, format);
-	if (!(op = (t_format *)malloc(sizeof(t_format))))
-		return (-1);
-	ft_init_list(op);
-	cnt = start_printf(format, ap, op);
+	cnt = start_printf(format, ap);
 	va_end(ap);
 	return (cnt);
 }
