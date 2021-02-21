aa15916031719a06a3222dcb39557b7d4c1a7dfe
kwon <gurqo1223@gmail.com>
2021-02-21 19:59:36 +0900

    create_ft_print_char

--- ft_printf/ft_print_char.c
+++ ft_printf/ft_print_char.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 21:26:34 by hkwon             #+#    #+#             */
-/*   Updated: 2021/02/04 16:46:26 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/02/21 19:46:02 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int	ft_print_char(va_list ap, t_format *op)
 {
-	
+	int cnt;
+	int s_width;
+
+	cnt = 0;
+	s_width = op->width - 1;
+	if (op->left == 1)
+	{
+		cnt += ft_putchar(va_arg(ap, int));
+		while (s_width-- > 0)
+			cnt += ft_putchar(' ');
+	}
+	else
+	{
+		while (s_width-- > 0)
+			cnt += ft_putchar(' ');
+		cnt += ft_putchar(va_arg(ap, int));
+	}
+	return (cnt);
 }
