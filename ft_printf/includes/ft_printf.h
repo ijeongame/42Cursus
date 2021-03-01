7b9b99b7beb1efaafc743b4bde8994442f9b391f
kwon <gurqo1223@gmail.com>
2021-03-01 23:03:27 +0900

    ft_printf_header

--- ft_printf/includes/ft_printf.h
+++ ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 15:54:17 by hkwon             #+#    #+#             */
-/*   Updated: 2021/02/23 23:26:32 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/03/01 22:41:24 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include "libft/libft.h"
+# include "../libft/libft.h"
 
 typedef struct			s_format{
 	int			left;
@@ -30,11 +30,12 @@ typedef struct			s_format{
 int						ft_printf(const char *format, ...);
 void					ft_init_list(t_format *op);
 
-
+int						ft_print_char(va_list ap, t_format *op);
+int						ft_print_str(va_list ap, t_format *op);
+int						ft_print_ptr(va_list ap, t_format *op);
 int						ft_print_int(va_list ap, t_format *op);
 int						ft_print_un_int(va_list ap, t_format *op);
-int						ft_print_char(va_list ap, t_format *op);
 int						ft_print_hex(va_list ap, t_format *op);
-int						ft_print_ptr(va_list ap, t_format *op);
+int						ft_print_per(t_format *op);
 
 #endif
