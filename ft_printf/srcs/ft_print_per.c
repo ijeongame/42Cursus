0d4ac6612f069d0e6f8f429bfc1d890793ef0086
kwon <gurqo1223@gmail.com>
2021-03-01 23:02:17 +0900

    create_print_precent

--- ft_printf/srcs/ft_print_per.c
+++ ft_printf/srcs/ft_print_per.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 18:22:35 by hkwon             #+#    #+#             */
-/*   Updated: 2021/02/21 20:56:02 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/03/01 22:53:33 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,22 @@ int		ft_print_per(t_format *op)
 
 	cnt = 0;
 	s_width = op->width - 1;
+	if (op->left)
+	{
+		cnt += ft_putchar('%');
+		while (s_width-- > 0)
+			cnt += ft_putchar(' ');
+	}
+	else
+	{
+		while (s_width-- > 0)
+		{
+			if (op->zero)
+				cnt += ft_putchar('0');
+			else
+				cnt += ft_putchar(' ');
+		}
+		cnt += ft_putchar('%');
+	}
+	return (cnt);
 }
