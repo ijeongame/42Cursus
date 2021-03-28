03add05fd7eb2abf0c84571ed264a73e1eb1e0ca
kwon <gurqo1223@gmail.com>
2021-03-29 00:03:23 +0900

    add_bash

--- 02_ft_server/srcs/run.sh
+++ 02_ft_server/srcs/run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#/bin/bash -C /set_autoindex.sh
-bash set_autoindex.sh
+/bin/bash -C /set_autoindex.sh
 
 service nginx start
 service mysql start
 service php7.3-fpm start
-
-# nginx -g "daemon off;"
+bash
