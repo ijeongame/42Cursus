568eb612d09f6c0863c1b505b4c34b9256d66d36
hb-kwon <gurqo1223@gmail.com>
2021-05-23 19:21:22 +0900

    push_swap_start

---
+++ 02_push_swap/libft/srcs/ft_strcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/28 02:10:03 by hkwon             #+#    #+#             */
+/*   Updated: 2021/04/28 02:10:06 by hkwon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	int				n;
+	unsigned char	u1;
+	unsigned char	u2;
+
+	if ((n = ft_strlen(s1)) != (int)ft_strlen(s2))
+		return (1);
+	while (n-- > 0)
+	{
+		u1 = *(unsigned char *)s1++;
+		u2 = *(unsigned char *)s2++;
+		if (u1 != u2)
+			return (u1 - u2);
+		if (u1 == '\0')
+			return (0);
+	}
+	return (0);
+}
