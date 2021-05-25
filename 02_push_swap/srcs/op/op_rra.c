2287b3042e92844c0ee6bc3b481376218678ca2c
hb-kwon <gurqo1223@gmail.com>
2021-05-25 23:42:01 +0900

    make_operation_rra_rrb_rrr

--- 02_push_swap/srcs/op/op_rra.c
+++ 02_push_swap/srcs/op/op_rra.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 19:32:38 by hkwon             #+#    #+#             */
-/*   Updated: 2021/05/25 23:11:30 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/05/25 23:40:41 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,10 @@ void	op_rra(t_lst **a, t_info *info)
 
 	if (info->a_size < 2)
 		return ;
+	node = move_stat(*a);
+	tmp = node->val;
+	node->next->prev = 0;
+	free(node);
+	(*a)->next = new_lst_tail(*a, tmp);
+	(*a) = (*a)->next;
 }
--- 02_push_swap/srcs/op/op_rrb.c
+++ 02_push_swap/srcs/op/op_rrb.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 19:33:40 by hkwon             #+#    #+#             */
-/*   Updated: 2021/05/25 22:14:48 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/05/25 23:41:07 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,15 @@
 
 void	op_rrb(t_lst **b, t_info *info)
 {
+	t_lst	*node;
+	long	tmp;
 
+	if (info->b_size < 2)
+		return ;
+	node = move_stat(*b);
+	tmp = node->val;
+	node->next->prev = 0;
+	free(node);
+	(*b)->next = new_lst_tail(*b, tmp);
+	(*b) = (*b)->next;
 }
