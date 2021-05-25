f04303429e59ea85ca175c72fa8a09b26bceedd5
hb-kwon <gurqo1223@gmail.com>
2021-05-26 00:43:00 +0900

    make_operation_pa_pb

--- 02_push_swap/srcs/op/op_pa.c
+++ 02_push_swap/srcs/op/op_pa.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 19:12:22 by hkwon             #+#    #+#             */
-/*   Updated: 2021/05/25 23:05:52 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/05/26 00:42:14 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	op_pa(t_lst **a, t_lst **b, t_info *info)
 {
-
+	if (!info->b_size)
+		return ;
+	if (!(*a))
+		*a = new_lst_tail(0, (*b)->val);
+	else
+	{
+		(*a)->next = new_lst_tail(*a, (*b)->val);
+		(*a) = (*a)->next;
+		delete_lst(**b);
+	}
+	info->a_size++;
+	info->b_size--;
 }
--- 02_push_swap/srcs/op/op_pb.c
+++ 02_push_swap/srcs/op/op_pb.c
@@ -6,14 +6,24 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 19:31:38 by hkwon             #+#    #+#             */
-/*   Updated: 2021/05/25 19:34:21 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/05/26 00:38:08 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
 void	op_pb(t_lst **a, t_lst **b, t_info *info)
 {
-
+	if (!info->a_size)
+		return ;
+	if (!(*b))
+		*b = new_lst_tail(0, (*a)->val);
+	else
+	{
+		(*b)->next = new_lst_tail(*b, (*a)->val);
+		(*b) = (*b)->next;
+		delete_lst(**a);
+	}
+	info->a_size++;
+	info->b_size--;
 }
