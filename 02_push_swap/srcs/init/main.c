18d4b22b1674f3969115ccb683319a078d14e97b
hb-kwon <gurqo1223@gmail.com>
2021-06-01 00:40:15 +0900

    push_swap_main

--- 02_push_swap/srcs/init/main.c
+++ 02_push_swap/srcs/init/main.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 16:58:55 by hkwon             #+#    #+#             */
-/*   Updated: 2021/05/30 22:21:04 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/05/31 23:33:11 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	ps_info_init(t_info *info)
 	info->min = 0;
 	info->pivot = 0;
 	info->pos = 0;
-	info->cnt = 0;
 	info->a_size = 0;
 	info->b_size = 0;
 }
