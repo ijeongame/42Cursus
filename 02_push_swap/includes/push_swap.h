a35a11a1457f17979de9b5848161a6bdf9250bfe
hb-kwon <gurqo1223@gmail.com>
2021-06-12 14:09:48 +0900

    push_swap_header

--- 02_push_swap/includes/push_swap.h
+++ 02_push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 19:15:14 by hkwon             #+#    #+#             */
-/*   Updated: 2021/06/01 06:09:17 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/06/11 23:22:42 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include "libft.h"
+# include "../libft/includes/libft.h"
 # include "push_swap_struct.h"
 # include "push_swap_op.h"
 # include "push_swap_utils.h"
