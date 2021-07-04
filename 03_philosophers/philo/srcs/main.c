5f1719ab6c4036952170179b6de6e861c57f1b4e
hb-kwon <gurqo1223@gmail.com>
2021-07-04 20:01:54 +0900

    philosophers_main_add_init_func

--- 03_philosophers/philo/srcs/main.c
+++ 03_philosophers/philo/srcs/main.c
@@ -6,21 +6,31 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 16:14:23 by hkwon             #+#    #+#             */
-/*   Updated: 2021/07/01 21:58:04 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/07/04 19:39:12 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int ac, int *av[])
+static void	init_philo(t_info *info, int ac, char *av[])
+{
+	info->num_of_philo = ft_atoi(av[1]);
+	info->time_to_die = ft_atoi(av[2]);
+	info->time_to_eat = ft_atoi(av[3]);
+	info->time_to_sleep = ft_atoi(av[4]);
+	if (ac == 5)
+		info->num_must_eat = -1;
+	else
+		info->num_must_eat = ft_atoi(av[5]);
+}
+
+int			main(int ac, char *av[])
 {
 	t_info	info;
 
 	if (ac != 5 && ac != 6)
 		return (0);
-	info.num_of_philo = av[1];
-	info.time_to_die = av[2];
-	info.time_to_eat = av[3];
-	info.time_to_sleep = av[4];
-
+	memset(&info, 0, sizeof(info));
+	init_philo(&info, ac, av);
+	print(info);
 }
