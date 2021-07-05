98cfb66c66a4abba3f1c3c7f7af6738dd65a4797
hb-kwon <gurqo1223@gmail.com>
2021-07-05 20:06:43 +0900

    philosophers_main_add_start_philo

--- 03_philosophers/philo/srcs/main.c
+++ 03_philosophers/philo/srcs/main.c
@@ -6,23 +6,11 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 16:14:23 by hkwon             #+#    #+#             */
-/*   Updated: 2021/07/04 19:39:12 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/07/05 20:02:43 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-static void	init_philo(t_info *info, int ac, char *av[])
-{
-	info->num_of_philo = ft_atoi(av[1]);
-	info->time_to_die = ft_atoi(av[2]);
-	info->time_to_eat = ft_atoi(av[3]);
-	info->time_to_sleep = ft_atoi(av[4]);
-	if (ac == 5)
-		info->num_must_eat = -1;
-	else
-		info->num_must_eat = ft_atoi(av[5]);
-}
 
 int			main(int ac, char *av[])
 {
