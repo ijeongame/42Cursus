f77de67b84d37035a2896a36a4581cc7e385fa8c
hb-kwon <gurqo1223@gmail.com>
2021-07-05 21:38:36 +0900

    philosophers_main_add_start_philo

--- 03_philosophers/philo/srcs/main.c
+++ 03_philosophers/philo/srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 16:14:23 by hkwon             #+#    #+#             */
-/*   Updated: 2021/07/05 20:02:43 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/07/05 20:09:24 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int			main(int ac, char *av[])
+int	main(int ac, char *av[])
 {
 	t_info	info;
 
@@ -20,5 +20,4 @@ int			main(int ac, char *av[])
 		return (0);
 	memset(&info, 0, sizeof(info));
 	init_philo(&info, ac, av);
-	print(info);
 }
