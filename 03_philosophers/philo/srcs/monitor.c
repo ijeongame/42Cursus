b13ba00f461edebc9243e3f67d7b6671be3211b9
hb-kwon <gurqo1223@gmail.com>
2021-07-07 18:12:49 +0900

    philosophers_monitor_num_of_must_eat_condition

--- 03_philosophers/philo/srcs/monitor.c
+++ 03_philosophers/philo/srcs/monitor.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 19:21:19 by hkwon             #+#    #+#             */
-/*   Updated: 2021/07/06 22:40:49 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/07/07 17:08:40 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,16 @@ void	*monitor(void *av)
 	{
 		pthread_mutex_lock(&philo->eat_mutex);
 		pthread_mutex_lock(&philo->info->fin_mutex);
+		gettimeofday(&time, NULL);
 		ms = get_time(time) - get_time(philo->last_eat_time);
+		gettimeofday(&time, NULL);
 		if (ms >= philo->info->time_to_die)
 		{
+			printf("%lld %d died\n", get_time(time) - \
+			get_time(philo->info->eat_time), philo->n + 1);
+			philo->info->finish = 1;
 		}
+		pthread_mutex_unlock(&philo->eat_mutex);
+		pthread_mutex_unlock(&philo->info->fin_mutex);
 	}
 }
