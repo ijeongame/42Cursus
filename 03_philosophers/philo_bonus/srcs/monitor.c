3e81ed6e72498e1e2855fb0b4d6e9fdb8eb99d92
hb-kwon <gurqo1223@gmail.com>
2021-08-05 14:18:20 +0900

    philosophers_philo_bonus_norm_check_fin

--- 03_philosophers/philo_bonus/srcs/monitor.c
+++ 03_philosophers/philo_bonus/srcs/monitor.c
@@ -6,7 +6,7 @@
 /*   By: kwonhyukbae <kwonhyukbae@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 20:11:40 by kwonhyukbae       #+#    #+#             */
-/*   Updated: 2021/08/05 13:44:04 by kwonhyukbae      ###   ########.fr       */
+/*   Updated: 2021/08/05 14:18:09 by kwonhyukbae      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	*monitor_full(void *arg)
 	if (info->finish == DIED)
 		return (NULL);
 	print_msg(info->philo, FULL);
-	sem_post(info->died); //이제 다 먹었을때 종료하는 시그널을 보내줘야하므로 끝을 post.
+	sem_post(info->died);
 	return (NULL);
 }
 
