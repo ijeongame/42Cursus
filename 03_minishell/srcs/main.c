b7d84cc9cd83c4e2d09d0f7008c65e2c31adfdc0
Hyuk Bae Kwon <67624284+hb-kwon@users.noreply.github.com>
2021-09-08 16:35:18 +0900

    Minishell init env

--- 03_minishell/srcs/main.c
+++ 03_minishell/srcs/main.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kwonhyukbae <kwonhyukbae@student.42.fr>    +#+  +:+       +#+        */
+/*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 10:59:00 by kwonhyukbae       #+#    #+#             */
-/*   Updated: 2021/09/04 12:49:57 by kwonhyukbae      ###   ########.fr       */
+/*   Updated: 2021/09/08 04:48:07 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// 환경변수를 조작할 시 malloc과 free에 있어서 main 변수 envp를 그대로 사용하면
+// error가 발생한다고 한다.
 void	init_env(char ***en, char *envp[])
 {
 	int	i;
@@ -36,13 +38,17 @@ void	init_env(char ***en, char *envp[])
 
 int	main(int argc, char *argv[], char *envp[])
 {
+	// 환경변수를 따로 저장할 문자열을 만들 것인가?
+	// 환경변수를 바로 사용할 것인가?
+	// 바로 사용했을 때 문제가 될 부분은 없는가?
+	// 수정할 일이 생기면 거기에 맞춰서 작성.
 	char	**en;
 
-	while (*envp)
-		printf("%s\n", *envp++);
 	init_env(&en, envp);
 	minishell(en);
 	(void)argc;
 	(void)argv;
 	return (0);
 }
+
+// 언제 error기 1이 나오고 0이 나오는지
