1c6aa6fcc9191aeed887d2c096a819325aff995c
Hyuk Bae Kwon <67624284+hb-kwon@users.noreply.github.com>
2021-09-08 16:39:10 +0900

    Minishell header & Thinking about the structure

--- 03_minishell/includes/minishell.h
+++ 03_minishell/includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kwonhyukbae <kwonhyukbae@student.42.fr>    +#+  +:+       +#+        */
+/*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 11:10:37 by kwonhyukbae       #+#    #+#             */
-/*   Updated: 2021/09/04 14:37:22 by kwonhyukbae      ###   ########.fr       */
+/*   Updated: 2021/09/08 16:09:26 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,59 @@
 # define PATH_MAX 1024
 # define BLTIN_NUM 7
 
-typedef struct s_minishell
-{
-}	t_minishell;
+// typedef struct s_minishell	t_minishell;
+// typedef struct s_cmd		t_cmd;
+// typedef struct s_env		t_env;
+
+// 하면서 필요한 부분을 구조체에 넣어서 
+// struct s_minishell
+// {
+// 	t_cmd	cmd;
+// };
+
+// 환경변수를 저장할 필요가 있다면 key/value값을 나눠 저장하자.
+// "="을 기준으로 나눈다.
+// struct s_env
+// {
+// 	char	*key;
+// 	char	*value;
+// 	t_env	*next;
+// };
+
+// cmd struct
+// struct s_cmd
+// {
+// 	char	*cmd;
+// 	t_list	*args;
+// };
 
 int		main(int argc, char *argv[], char *envp[]);
 void	minishell(char **en);
 
 /*
-builtin commands
+** execute commands
+*/
+char	**execute(char **args, char **en);
+
+/*
+** builtin commands
 */
 char	*blt_str(int i);
-int		(*blt_func(int i))(char *line, char **en);
-int		ft_echo(char *line, char **en);
-int		ft_cd(char *line, char **en);
-int		ft_pwd(char *line, char **en);
-int		ft_export(char *line, char **en);
-int		ft_unset(char *line, char **en);
-int		ft_env(char *line, char **en);
-int		ft_exit(char *line, char **en);
+char	**(*blt_func(int i))(char **args, char **en);
+
+/*
+** builtin function
+*/
+char	**ft_echo(char **args, char **en);
+char	**ft_cd(char **args, char **en);
+char	**ft_pwd(char **args, char **en);
+char	**ft_export(char **args, char **en);
+char	**ft_unset(char **args, char **en);
+char	**ft_env(char **args, char **en);
+char	**ft_exit(char **args, char **en);
+
+// utils
+char	**ft_token(char *s, char *sep);
+char	*ft_strnew(size_t size);
 
 #endif
