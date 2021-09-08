bc39b5344f40105a24959e832df5c9daab52689b
Hyuk Bae Kwon <67624284+hb-kwon@users.noreply.github.com>
2021-09-08 16:37:52 +0900

    Minishell create builtin func call using function pointer

--- 03_minishell/srcs/builtin.c
+++ 03_minishell/srcs/builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kwonhyukbae <kwonhyukbae@student.42.fr>    +#+  +:+       +#+        */
+/*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 13:26:18 by kwonhyukbae       #+#    #+#             */
-/*   Updated: 2021/09/04 14:42:53 by kwonhyukbae      ###   ########.fr       */
+/*   Updated: 2021/09/08 15:02:53 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,14 @@ char	*blt_str(int i)
 }
 
 // 함수포인터를 이용하여 내장 기능을 수행한다.
-int	(*blt_func(int i))(char *line, char **en)
+// 함수포인터를 사용하는 이유는 ?
+// exit pwd cd
+// echo env unset export
+// 이차원배열을 만들어서 char *line -> char **cmd로 내장함수를 저장할 수 있게 만들자.
+// 함수포인터를 사용했을 때 인자값이 고정이 되어버린다.
+char	**(*blt_func(int i))(char **args, char **en)
 {
-	int	(*blt_func[BLTIN_NUM])(char *line, char **en);
+	char	**(*blt_func[BLTIN_NUM])(char **args, char **en);
 
 	blt_func[0] = &ft_echo;
 	blt_func[1] = &ft_cd;
