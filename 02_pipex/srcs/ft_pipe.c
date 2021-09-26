37776c8c0d276f3b287fcb76ee565f0290b82c6a
Hyuk Bae Kwon <67624284+hb-kwon@users.noreply.github.com>
2021-09-26 15:53:52 +0900

    Pipex norm check

--- 02_pipex/srcs/ft_pipe.c
+++ 02_pipex/srcs/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 21:42:56 by hkwon             #+#    #+#             */
-/*   Updated: 2021/09/26 14:59:28 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/09/26 15:53:27 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,6 @@ int	main(int argc, char *argv[], char *envp[])
 
 	if (argc != 5)
 		ft_pipe_exit("Check Argument: ./pipex file1 cmd1 cmd2 file2\n");
-	//다음 if문이 실행된다는 의미는 pipe가 제대로 생성되지 않았다는 것을 의미
-	//pipe가 생성됐다면 pipe_fd[0]은 읽기용 파일 디스크립터, pipe_fd[1]은 쓰기용 파일 디스크립터가 된다.
 	ft_pipe_process(&info, argv, envp);
 	return (0);
 }
