3000e1c232ec608f2cdcadce31f030c8edaae5ff
hb-kwon <gurqo1223@gmail.com>
2022-01-09 23:07:15 +0900

    Cpp Module 01

--- 04_CPP_Module/module_01/ex03/Weapon.hpp
+++ 04_CPP_Module/module_01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:16:44 by hkwon             #+#    #+#             */
-/*   Updated: 2022/01/04 22:50:11 by hkwon            ###   ########.fr       */
+/*   Updated: 2022/01/09 23:02:19 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 class Weapon{
 	private:
 		std::string	type;
-	public:
 		Weapon(void);
+	public:
 		Weapon(std::string type);
 		const	std::string &getType(void);
 		void	setType(std::string type);
