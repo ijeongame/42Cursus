fe2359cc5c9887ed5934a43f86a9b7c6c9390676
hb-kwon <gurqo1223@gmail.com>
2022-02-08 19:42:55 +0900

    change folder name

--- 04_CPP_Module/module_02/ex02/main.cpp
+++ 04_CPP_Module/module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 00:21:52 by hkwon             #+#    #+#             */
-/*   Updated: 2021/12/30 23:07:28 by hkwon            ###   ########.fr       */
+/*   Updated: 2022/01/11 10:06:37 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,46 @@
 
 int main( void )
 {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	// Fixed a;
+	// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
+	// std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+
+	// std::cout << b << std::endl;
+
+	// std::cout << Fixed::max( a, b ) << std::endl;
+
+	// return 0;
+	Fixed a(1);
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+	Fixed c(1.0f);
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-
 	std::cout << b << std::endl;
-
-	std::cout << Fixed::max( a, b ) << std::endl;
-
-	return 0;
+	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << "----------------------" << std::endl;
+	std::cout << --a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << a << std::endl;
+	std::cout << Fixed::min(a, b) << std::endl;
+	std::cout << "------comparison------" << std::endl;
+	std::cout << (a < b) << std::endl;
+	std::cout << (a > b) << std::endl;
+	std::cout << (a <= c) << std::endl;
+	std::cout << (a >= c) << std::endl;
+	std::cout << (a == c) << std::endl;
+	std::cout << (a != b) << std::endl;
+	std::cout << "------arithmetic------" << std::endl;
+	std::cout << a + b << std::endl;
+	std::cout << a - b << std::endl;
+	std::cout << a * b << std::endl;
+	std::cout << a / b << std::endl;
 }
