f1d5b9e790021814afed7773b55e4c9c95dcad16
hb-kwon <gurqo1223@gmail.com>
2022-03-01 16:37:34 +0900

    Inception test virtual

--- 05_inception/srcs/requirements/nginx/tools/setup.sh
+++ 05_inception/srcs/requirements/nginx/tools/setup.sh
@@ -2,7 +2,7 @@
 
 openssl req -newkey rsa:2048 -days 365 -nodes -x509 \
     -subj "/C=KR/ST=Seoul/L=Gaepo/O=42Seoul/OU=hkwon/CN=localhost" \
-    -keyout /etc/ssl/private/inception_server.key -out /etc/ssl/certs/inception_server.crt;
+    -keyout /etc/ssl/private/inception_server_pkey.key -out /etc/ssl/certs/inception_server.crt;
 
 cp ./default    /etc/nginx/sites-available/default
 
--- 05_inception/srcs/requirements/wordpress/tools/setup.sh
+++ 05_inception/srcs/requirements/wordpress/tools/setup.sh
@@ -4,18 +4,21 @@
 # if문 안의 내용들은 처음에만 실행되면 되기 때문에 if문 걸어줌.
 if [ ! -e /var/www/html/index.php ]; then
   # wordpress setting
-  cp -R /wordpress/* /var/www/html/
+  cp -r /wordpress/* /var/www/html/
+  chown -R www-data:www-data /var/www/html
   rm /var/www/html/wp-config-sample.php
   cp wp-config.php /var/www/html/wp-config.php
-  chown -R www-data:www-data /var/www/html
 
   # wordpress 설치
   wp core download --locale=ko_KR --allow-root
-  until wp core install --url=$DOMAIN_NAME --title=HelloWP --admin_user=$WP_USER1_ID --admin_password=$WP_USER1_PASSWORD --admin_email=hkwon@student.42seoul.com --allow-root --skip-email --path='/var/www/html/'; do
+  until
+  wp core install --allow-root --url=$DOMAIN_NAME --title='Hello_WordPress' --admin_user=$WP_USER1_ID\
+		--admin_password=$WP_USER1_PASSWORD --admin_email="hkwon@student.42seoul.kr" --path='/var/www/html'; do
   sleep 0.5
   done
   # wordpress user 생성
-  wp user create --allow-root $WP_USER2_ID ijeong@student.42seoul.fr --user_pass=$WP_USER2_PASSWORD --role=author
+  wp user create --allow-root $WP_USER2_ID ijeong@student.42seoul.kr --user_pass=$WP_USER2_PASSWORD --role=author --path='/var/www/html'
+
 fi
 
 service php7.3-fpm stop
