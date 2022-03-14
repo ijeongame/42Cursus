057165922fd90810680d05aa734c50d5d333b99c
hb-kwon <gurqo1223@gmail.com>
2022-03-14 14:59:14 +0900

    Inception check for eva

--- 05_inception/srcs/requirements/wordpress/tools/setup.sh
+++ 05_inception/srcs/requirements/wordpress/tools/setup.sh
@@ -2,7 +2,6 @@
 
 # 이 shell파일은 컨테이너가 올라갈때마다 실행이 되는데,
 # if문 안의 내용들은 처음에만 실행되면 되기 때문에 if문 걸어줌.
-sleep 10;
 if [ ! -e /var/www/html/index.php ]; then
   # wordpress setting
   cp -r /wordpress/* /var/www/html/
@@ -15,12 +14,10 @@ if [ ! -e /var/www/html/index.php ]; then
 
   # wordpress 설치
   wp core download --locale=ko_KR --allow-root
-  # wp config create \
-  #   --dbname=$MYSQL_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb \
-  #   --locale=ko_KR
-
-  wp core install --url=$DOMAIN_NAME --allow-root --title='Hello_WordPress' --admin_user=$WP_USER1_ID\
-    --admin_password=$WP_USER1_PASSWORD --admin_email="hkwon@student.42seoul.kr" --path='/var/www/html'
+  until wp core install --url=$DOMAIN_NAME --allow-root --title='Hello_WordPress' --admin_user=$WP_USER1_ID\
+    --admin_password=$WP_USER1_PASSWORD --admin_email="hkwon@student.42seoul.kr" --path='/var/www/html'; do
+  sleep 0.5;
+  done
 
   # wordpress user 생성
   wp user create --allow-root $WP_USER2_ID ijeong@student.42seoul.kr --user_pass=$WP_USER2_PASSWORD --role=author --path='/var/www/html'
