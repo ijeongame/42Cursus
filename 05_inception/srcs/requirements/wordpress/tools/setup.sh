d36076841f1859d3f131ddeb1f10655c3ff13ba2
hb-kwon <gurqo1223@gmail.com>
2022-03-17 15:35:52 +0900

    Inception eva -ing

--- 05_inception/srcs/requirements/wordpress/tools/setup.sh
+++ 05_inception/srcs/requirements/wordpress/tools/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# 이 shell파일은 컨테이너가 올라갈때마다 실행이 되는데,
-# if문 안의 내용들은 처음에만 실행되면 되기 때문에 if문 걸어줌.
 if ! wp core is-installed --allow-root --path='/var/www/html'; then
-  # wordpress 설치
   wp core download --locale=ko_KR --allow-root --path='/var/www/html'
   wp config create --dbname=$MYSQL_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --locale=ko_KR --allow-root --path='/var/www/html'
   until wp core install --url=$DOMAIN_NAME --allow-root --title='Hello_WordPress' --admin_user=$WP_USER1_ID\
@@ -11,11 +8,9 @@ if ! wp core is-installed --allow-root --path='/var/www/html'; then
   sleep 0.5;
   done
 
-  # wordpress user 생성
   wp user create --allow-root $WP_USER2_ID ijeong@student.42seoul.kr --user_pass=$WP_USER2_PASSWORD --role=author --path='/var/www/html'
   chown -R www-data:www-data /var/www/html
 fi
 
 service php7.3-fpm stop
-# wordpress를 foreground로 돌리기
 php-fpm7.3 --nodaemonize
